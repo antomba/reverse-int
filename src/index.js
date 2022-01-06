4f470c7d134eec8500f5c3bfb9abd55e805eb3fa
Andrei <andrei@planet.ee>
2022-01-06 23:09:44 +0200

    init: add reverse-int task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    var result = "";
+    for (let i = n.toString().length; i > 0; i--) {
+        result += n.toString()[i - 1];
+    }
+    return parseInt(result);
+};
